df117a80353e07e1b4e496fa381069db5f6c6c8a
root <neginsky58@gmail.com>
2014-04-14 21:54:13 +0300

    first build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pimovation::Application.config.secret_key_base = '1bfbc27cca0afe08e325fe70f1e0a30a2e99def3e9f985403766843710c5c890804c00c9710d5cb8145b5d3aa8717bd93a7e359cd1aa9e1678f5f4821f98d46b'
