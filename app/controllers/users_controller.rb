1069c06d7d1a5782ed5c82a92e11e28f579dbb4a
root <neginsky58@gmail.com>
2014-04-22 17:25:40 +0300

    merged

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
 
+  before_filter :authenticate_user!
+  
   def profile
-    
+    @profile = current_user.user_profile
+  end
+
+  def update_profile
+    @profile = params[:profile]
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140416062726) do
+ActiveRecord::Schema.define(version: 20140422115420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -135,6 +135,7 @@ ActiveRecord::Schema.define(version: 20140416062726) do
     t.integer  "preference_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "user_types", force: true do |t|
